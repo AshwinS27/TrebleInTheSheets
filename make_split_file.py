ce78235030d547935200d2e5ef0e7748bea3e011
drewskis <drewskis@umich.edu>
2022-04-21 17:09:18 -0400

    Basic segmentation
    Added split.json maker too

---
+++ make_split_file.py
@@ -0,0 +1,45 @@
+import os
+import json
+import numpy as np
+
+###### META VARIABLES #####
+NUM_TRAIN = 5
+NUM_VALID = 1
+NUM_TEST = 1
+###########################
+
+path_to_images = './ds2_dense/images_small/'
+
+# get image names
+image_names = []
+for name in os.listdir(path_to_images):
+    if '.png' in name:
+        image_names.append(name)
+
+
+dict = {
+    "train": [],
+    "validation": [],
+    "test": []
+}
+
+i = 0
+for f_name in image_names:
+    image_name = f_name
+    ground_truth = f_name.replace(".png", "_seg.png")
+    GT_quantize = ground_truth + ".npy"
+    arr = [image_name, ground_truth, GT_quantize]
+
+    if (i < NUM_TRAIN):
+        dict["train"].append(arr)
+    elif (i < NUM_TRAIN + NUM_VALID):
+        dict["validation"].append(arr)
+    elif (i < NUM_TRAIN + NUM_VALID + NUM_TEST):
+        dict["test"].append(arr)
+
+    i+=1 
+
+
+#TURN TO JSON OBJECT AND PRINT
+json_object = json.dumps(dict, indent = 4) 
+print(json_object)
